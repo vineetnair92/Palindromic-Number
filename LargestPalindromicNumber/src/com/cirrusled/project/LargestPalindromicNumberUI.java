4da95ca2a4d4d921c342fee54b679afc9d5cf850
Vineet Nair <Vineet Nair@10.0.0.93>
2018-04-14 20:24:58 -0700

    Event Handler & Input Validation

--- LargestPalindromicNumber/src/com/cirrusled/project/LargestPalindromicNumberUI.java
+++ LargestPalindromicNumber/src/com/cirrusled/project/LargestPalindromicNumberUI.java
@@ -11,24 +11,79 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
-
 import java.util.ArrayList;
 
+
 public class LargestPalindromicNumberUI extends Application {
+	
+	/*  Function to check input values
+	 *  @param minInput lower bound number
+	 *  @param maxInput higher bound number
+	 *  @return boolean indicating if range is valid or not
+	 */
+	public boolean ValidateFields(TextField minInput, TextField maxInput ){
+
+		double min=0,max=0;
+		boolean flag=true;
+		
+		try
+		{
+			minInput.setStyle(null);
+			//get Values from minimum input field
+			min= Double.parseDouble(minInput.getText());
+			
+		}
+		catch(NumberFormatException ex)
+		{
+				minInput.setStyle("-fx-border-color:red");
+				flag=false;
+		}
+
+		try
+		{
+			maxInput.setStyle(null);	
+			//get Value from maximum input field
+			max= Double.parseDouble(maxInput.getText());
+		}
+		catch(NumberFormatException e)
+		{
+			maxInput.setStyle("-fx-border-color:red");
+			flag=false;		
+		}
+
+		if(min<0 && max<0) {
+			return false;
+		}
+		//Find lower and upper bounds
+		if(max>min)	{
+			minInput.setText((long)min+"");
+			maxInput.setText((long)max+"");
+		}
+		else{
+			minInput.setText((long)max+"");
+			maxInput.setText((long)min+"");
+		}
+				 					
+		return flag;
+	 	}
+	
+
+	
 	
 	/* Function to build the grid layout
 	 * 
 	 *  @param grid:  The grid object argument
 	 *  @return grid object
 	 */
-	public GridPane BuildGrid(GridPane grid)
-		{
-			grid.setPadding(new Insets(20, 20, 20, 20));
-			grid.setVgap(30);
-			grid.setHgap(30);		
-			return grid;
-		}
+	public GridPane BuildGrid(GridPane grid){
+		grid.setPadding(new Insets(20, 20, 20, 20));
+		grid.setVgap(30);
+		grid.setHgap(30);		
+		return grid;
+		
+	}
 				
+	
 	
 	/*
 	 * Function to add elements to grid.
@@ -71,7 +126,43 @@ public class LargestPalindromicNumberUI extends Application {
 		return nodes;
 		
 	}
+	
+	
+	
+	/*
+	 * Function to retrieve fields from UI.
+	 * 	 
+	 * @param nodes:  The List that stores each UI component
+	 */
+	public void RetrieveValues(ArrayList<Node> nodes) {
+		try
+		{
+			Button searchButton = (Button) nodes.get(6);
+			TextField minInput = (TextField) nodes.get(1);
+			TextField maxInput = (TextField) nodes.get(3);
+			TextField result = (TextField) nodes.get(5);
 		
+			searchButton.setOnAction( clickEvent -> 
+			{
+				if(ValidateFields(minInput,maxInput))
+				{
+					//TODO: Get Palindromic Number
+					result.setText("");
+				}
+				else
+				{
+					result.setText("Invalid Values");
+					result.setStyle("-fx-text-fill: red; -fx-font-size: 16; -fx-font-weight: bold;");
+				}
+			});
+		}
+		catch(ClassCastException ex)
+		{
+			System.out.println("Cast Exception");
+		}
+	}
+	
+	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception{
@@ -87,16 +178,23 @@ public class LargestPalindromicNumberUI extends Application {
 		//Add all the UI Components to the grid
 		gp.getChildren().addAll(nodes);
 		
+		//Get values from input fields
+		RetrieveValues(nodes);
+		
 		//Display
 		Scene scene= new Scene(gp, 500,500);
 		primaryStage.setScene(scene);
 		primaryStage.show();	
-		}
 	
+	}
 	
+
+	
+	/*Main function
+	 * @param args: an Array of command line arguments
+	 */
 	public static void main(String args[]) {
 		launch(args);
 	}
-	
-	}
- 	
+		
+}
